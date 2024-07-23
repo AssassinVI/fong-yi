f1d64a76f337e53abe03dcb4d5442950c295ba0d
Louis <p19970408@gmail.com>
2024-07-23 11:07:30 +0800

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
   plugins: [
     vue(),
     AutoImport({
