47744e53ab091252667760493e951ec01847ef16
Louis <p19970408@gmail.com>
2024-07-31 16:29:57 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "url";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
+import legacy from "@vitejs/plugin-legacy";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +20,9 @@ export default defineConfig({
         }),
         Components({
             resolvers: [ElementPlusResolver()],
+        }),
+        legacy({
+            targets: ["defaults", "not IE 11"],
         }),
     ],
     resolve: {
